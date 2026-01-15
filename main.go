7308e53702ce6fe804b52e05688019e9cb761d19
dylanm537 <mcquakerd@gmail.com>
2026-01-15 10:49:46 +0000

    Changed main

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 )
 
-// Use quotes for string values
 var CommitHash = "dev-build"
 
 func main() {
-	// Use quotes for format strings and consider adding \n
-	fmt.Printf("Starting %s\n", CommitHash)
+	if CommitHash == "dev-build" {
+		fmt.Println("Couldn't get commit hash")
+	} else {
+		fmt.Printf("Got the commit hash right here: %s", CommitHash)
+	}
 }
